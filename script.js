c1315b58b376efd6b89a2ce8cc9b85fc4f9980f5
Aaron AN <acangle10@gmail.com>
2025-10-21 18:45:53 -0700

    contact form

--- script.js
+++ script.js
@@ -1,16 +1,25 @@
 document.addEventListener("DOMContentLoaded", function () {
-  var submit = document.querySelector("#submit-btn");
-  submit.addEventListener("click", function () {
+  const form = document.getElementById("form");
+
+  form.addEventListener("submit", async (e) => {
+    var loader = document.querySelector("#loader");
+    var content = document.querySelector("#form-content");
+    var thanks = document.querySelector("#thanks");
+
+    e.preventDefault();
+
+    if (!form.reportValidity()) {
+      return;
+    }
+
+    loader.style.display = "flex";
+    content.style.display = "none";
+
     var company = document.querySelector("#company");
     var name = document.querySelector("#name");
     var email = document.querySelector("#email");
     var phone = document.querySelector("#phone");
     var message = document.querySelector("#message");
-
-    if (!name.value || !email.value || !message.value || !phone.value) {
-      console.log("please fill out all required fields");
-      return;
-    }
 
     var url =
       "https://lvm5rrbcxmyjt47mlyk75bkpoq0zshcl.lambda-url.us-east-2.on.aws/";
@@ -41,6 +50,8 @@ document.addEventListener("DOMContentLoaded", function () {
       })
       .then((data) => {
         console.log("Success:", data); // Handle the successful response data
+        loader.style.display = "none";
+        thanks.style.display = "flex";
       })
       .catch((error) => {
         console.error("Error during fetch:", error); // Handle any errors
